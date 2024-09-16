12b4b4dbb6ceaca9ef50970071b240cfca4c9000
QuangAnh <anhdq2109@gmail.com>
2024-09-16 19:10:06 +0700

    change variable name
    change first name character to lowercase

--- src/main/java/model/Account.java
+++ src/main/java/model/Account.java
@@ -6,104 +6,104 @@ import controller.Validate;
 import java.time.LocalDateTime;
 
 public class Account {
-    private int AccountID;
-    private int RoleID;
-    private String Email;
-    private String FirstName;
-    private String LastName;
-    private int BirthYear;
-    private int ContactInformationID;
-    private String Password;
-    private LocalDateTime Time;
-    private int StatusID;
+    private int accountID;
+    private int roleID;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private int birthYear;
+    private int contactInformationID;
+    private String password;
+    private LocalDateTime time;
+    private int statusID;
 
     public int getAccountID() {
-        return AccountID;
+        return accountID;
     }
 
     public void setAccountID(String accountID) throws Exception {
         Validate.checkString(accountID);
-        AccountID = Integer.parseInt(accountID);
+        this.accountID = Integer.parseInt(accountID);
     }
 
     public int getRoleID() {
-        return RoleID;
+        return roleID;
     }
 
     public void setRoleID(String roleID) throws Exception {
         Validate.checkString(roleID);
-        RoleID = Integer.parseInt(roleID);
+        this.roleID = Integer.parseInt(roleID);
     }
 
     public String getEmail() {
-        return Email;
+        return email;
     }
 
     public void setEmail(String email) throws Exception {
         Validate.checkString(email,1,100);
-        Email = email;
+        this.email = email;
     }
 
     public String getFirstName() {
-        return FirstName;
+        return firstName;
     }
 
     public void setFirstName(String firstName) throws Exception {
         Validate.checkString(firstName,1,100);
-        FirstName = firstName;
+        this.firstName = firstName;
     }
 
     public String getLastName() {
-        return LastName;
+        return lastName;
     }
 
     public void setLastName(String lastName) throws Exception {
         Validate.checkString(lastName,1,100);
-        LastName = lastName;
+        this.lastName = lastName;
     }
 
     public int getBirthYear() {
-        return BirthYear;
+        return birthYear;
     }
 
     public void setBirthYear(String birthYear) throws Exception {
         Validate.checkString(birthYear);
-        BirthYear = Integer.parseInt(birthYear);
+        this.birthYear = Integer.parseInt(birthYear);
     }
 
     public int getContactInformationID() {
-        return ContactInformationID;
+        return contactInformationID;
     }
 
     public void setContactInformationID(String contactInformationID) throws Exception {
         Validate.checkString(contactInformationID);
-        ContactInformationID = Integer.parseInt(contactInformationID);
+        this.contactInformationID = Integer.parseInt(contactInformationID);
     }
 
     public String getPassword() {
-        return Password;
+        return password;
     }
 
     public void setPassword(String password) throws Exception {
         Validate.checkString(password,6,Integer.MAX_VALUE);
         Encrypt encrypt = new Encrypt();
-        Password = encrypt.toHexString(encrypt.getSHA(password));
+        this.password = encrypt.toHexString(encrypt.getSHA(password));
     }
 
     public LocalDateTime getTime() {
-        return Time;
+        return time;
     }
 
     public void setTime(String time) throws Exception {
-        Time = Validate.checkDateTime(time);
+        this.time = Validate.checkDateTime(time);
     }
 
     public int getStatusID() {
-        return StatusID;
+        return statusID;
     }
 
     public void setStatusID(String statusID) throws Exception {
         Validate.checkString(statusID);
-        StatusID = Integer.parseInt(statusID);
+        this.statusID = Integer.parseInt(statusID);
     }
 }
