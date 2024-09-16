eafc83f00a16b3402d89877f9f498131a10caa0f
QuangAnh <anhdq2109@gmail.com>
2024-09-16 23:42:00 +0700

    update Validate class

--- src/main/java/controller/Validate.java
+++ src/main/java/controller/Validate.java
@@ -6,14 +6,14 @@ import java.time.format.DateTimeFormatter;
 public class Validate {
     //check string not null
     public static void checkString(String str) throws Exception {
-        if(str.isEmpty()) throw new Exception("String is empty");
+        if(str==null) throw new Exception("String is empty");
     }
 
     //check minLen <= string length value <= maxLen (minLen >= 1)
     public static void checkString(String str, int minLen, int maxLen) throws Exception {
         checkString(str);
         if(str.length()>maxLen) throw new Exception("String too long");
-        if(minLen > 1 && str.length()<minLen) throw new Exception("String too short");
+        if(str.length()<minLen) throw new Exception("String too short");
     }
 
     //check min <= number <= max
