4915bdd11d17a32466076e9ef21fcd0a9ec2f3cc
QuangAnh <anhdq2109@gmail.com>
2024-09-16 13:49:23 +0700

    add ContactInformation class model

---
+++ src/main/java/model/ContactInformation.java
@@ -0,0 +1,36 @@
+package model;
+
+import controller.Validate;
+
+public class ContactInformation {
+    private int contactInformationID;
+    private String address; // nvarchar(200) not null
+    private String phoneNumber; //len between 6 and 11
+
+    public int getContactInformationID() {
+        return contactInformationID;
+    }
+
+    public void setContactInformationID(String contactInformationID) throws Exception {
+        Validate.checkString(contactInformationID);
+        this.contactInformationID = Integer.parseInt(contactInformationID);
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) throws Exception {
+        Validate.checkString(address, 1, 200);
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) throws Exception {
+        Validate.checkString(phoneNumber,6,11);
+        this.phoneNumber = phoneNumber;
+    }
+}
