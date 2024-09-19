2c3103d44d873846aea8b0175f52083885a4a7c0
ducdmhe172047 <doduccoloa1@gmail.com>
2024-09-19 16:12:16 +0700

    implement register v2

---
+++ src/main/java/controller/LoginServlet.java
@@ -0,0 +1,65 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Cookie;
+import model.Account;
+import dal.AccountDAO;
+import jakarta.servlet.http.HttpSession;
+import dal.ContactInformationDAO;
+import model.ContactInformation;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet(name = "LoginServlet", urlPatterns = {"/login"})
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.sendRedirect(request.getContextPath() + "/nest-frontend/page-login.jsp");
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<String> errorMessages = new ArrayList<>();
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String remember=request.getParameter("rem");
+
+        Cookie cu=new Cookie("cuser",email);
+        Cookie cp=new Cookie("cpass",password);
+        Cookie cr=new Cookie("crem",remember);
+
+        if(remember!=null){
+            cu.setMaxAge(60*60*24*7);
+            cp.setMaxAge(60*60*24*7);
+            cr.setMaxAge(60*60*24*7);
+        }else{
+            cu.setMaxAge(0);
+            cp.setMaxAge(0);
+            cr.setMaxAge(0);
+        }
+        response.addCookie(cu);
+        response.addCookie(cp);
+        response.addCookie(cr);
+
+        AccountDAO dao=new AccountDAO();
+        Account a=dao.getAccountByEmailPassword(email, password);
+        if(a==null){
+            errorMessages.add("Username or Password invalid!!");
+            request.setAttribute("errorMessages", errorMessages);
+            request.getRequestDispatcher("error.jsp").forward(request, response);
+        }else{
+            HttpSession session = request.getSession();
+            session.setAttribute("account", a);
+            response.sendRedirect(request.getContextPath() + "/nest-frontend/home-page.jsp");
+        }
+    }
+}
