ebcdb7ef7d1b37e1e38f04b4982b0300ff33ad3d
QuangAnh <anhdq2109@gmail.com>
2024-09-19 19:38:55 +0700

    add RoleDAO

---
+++ src/main/java/dao/RoleDAO.java
@@ -0,0 +1,25 @@
+package dao;
+
+import model.Role;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class RoleDAO extends  DBContext{
+    @Override
+    protected Object getObjectByRs(ResultSet rs) throws Exception {
+        return new Role(rs.getInt("RoleID"),rs.getString("Name"));
+    }
+
+    public Role getRole(Integer roleID){
+        try{
+            PreparedStatement ps = connection.prepareStatement("select * from Role where RoleID=?");
+            ps.setInt(1,roleID);
+            return (Role)getObject(ps);
+        }catch (SQLException ex){
+            System.out.println(ex.getMessage());
+        }
+        return null;
+    }
+}
