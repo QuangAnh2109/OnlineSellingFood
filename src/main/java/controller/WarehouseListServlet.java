86b3c0e059b969d08b22c63c6efc6e59617e5b42
Oahtneik <142812011+Oahtneik@users.noreply.github.com>
2024-10-16 09:32:33 +0700

    add warehouse list

---
+++ src/main/java/controller/WarehouseListServlet.java
@@ -0,0 +1,39 @@
+package controller;
+
+import dal.*;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.*;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "WarehouseListServlet", value = "/warehouseList")
+public class WarehouseListServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ContactInformationDAO contactInfoDAO = new ContactInformationDAO();
+        WarehouseStatusDAO statusDAO = new WarehouseStatusDAO();
+        WarehouseDAO warehouseDAO = new WarehouseDAO();
+        List<Warehouse> warehouseList = warehouseDAO.getAllWarehouse();
+        for (Warehouse warehouse : warehouseList) {
+            ContactInformation contactInfo = contactInfoDAO.getContactInformationByContactID(warehouse.getContactInformationID());
+            WarehouseStatus status = statusDAO.getStatusByStatusID(warehouse.getStatusID());
+
+            request.setAttribute("contactInfo_" + warehouse.getContactInformationID(), contactInfo);
+            request.setAttribute("status_" + warehouse.getStatusID(), status);
+        }
+
+        request.setAttribute("warehouseList", warehouseList);
+        request.getRequestDispatcher("warehouseList.jsp").forward(request, response);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
