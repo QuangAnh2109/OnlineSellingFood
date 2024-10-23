7b46947f3c5f8ca7a6557fe91bafe7afeb0495a8
Quang Anh <anhdq2109@gmail.com>
2024-10-23 08:13:20 +0700

    update ProductDAO

--- src/main/java/dal/ProductDAO.java
+++ src/main/java/dal/ProductDAO.java
@@ -1,7 +1,10 @@
 package dal;
 
+import common.Encrypt;
+import model.Account;
 import model.Product;
 
+import java.security.NoSuchAlgorithmException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -9,18 +12,29 @@ import java.sql.SQLException;
 public class ProductDAO extends DBContext{
     @Override
     protected Object getObjectByRs(ResultSet rs) throws SQLException {
-        return new Product(rs.getInt(1)
-                ,rs.getInt(2)
-                ,rs.getInt(3)
-                ,rs.getInt(4)
-                ,rs.getInt(5)
-                ,rs.getInt(6)
-                ,rs.getInt(7)
-                ,rs.getInt(8)
-                ,rs.getInt(9)
-                ,rs.getInt(9)
-                ,rs.getString(10)
-                ,rs.getString(11));
+        return new Product(rs.getInt("ProductID")
+                ,rs.getInt("Price")
+                ,rs.getObject("DiscountID",Integer.class)
+                ,rs.getInt("Weight")
+                ,rs.getInt("CategoryID")
+                ,rs.getInt("ManufacturerID")
+                ,rs.getInt("OriginID")
+                ,rs.getInt("UnitID")
+                ,rs.getInt("CertificationID")
+                ,rs.getInt("StatusID")
+                ,rs.getString("Name")
+                ,rs.getString("Detail"));
+    }
+
+    public Product getProductByID(int productID){
+        try{
+            PreparedStatement ps = connection.prepareStatement("select ProductID,Price,DiscountID,Weight,CategoryID,ManufacturerID,OriginID,UnitID,CertificationID,StatusID,Name,Detail from Product where ProductID=?");
+            ps.setInt(1, productID);
+            return (Product)getObject(ps);
+        }catch (SQLException e){
+            logger.info(getClass().getName()+": "+e.getMessage());
+        }
+        return null;
     }
     public void deleteDiscount(int productID){
         String sql="UPDATE [dbo].[Product]\n" +
