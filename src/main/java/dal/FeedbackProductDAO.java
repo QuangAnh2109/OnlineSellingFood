1bc27fdc1a780bab9fd1e80c0447e6a5783dc4e2
Đỗ Đức <doduccoloa1@gmail.com>
2024-10-27 18:28:53 +0700

    tao html cho feedback

---
+++ src/main/java/dal/FeedbackProductDAO.java
@@ -0,0 +1,15 @@
+package dal;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class FeedbackProductDAO extends DBContext{
+    @Override
+    protected Object getObjectByRs(ResultSet rs) throws SQLException {
+        return null;
+    }
+
+
+
+}
