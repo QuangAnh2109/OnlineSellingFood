4383add05bd6bb2848db5a755cf260f9066897d3
Đỗ Đức <doduccoloa1@gmail.com>
2024-10-31 14:37:22 +0700

    reply comment done

--- src/main/java/controller/ProductDetailServlet.java
+++ src/main/java/controller/ProductDetailServlet.java
@@ -24,7 +24,7 @@ public class ProductDetailServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-     int productID = Integer.parseInt(request.getParameter("productID"));
+        int productID = Integer.parseInt(request.getParameter("productID"));
 
 
         // Assuming you have a ProductDAO to fetch product details
@@ -39,11 +39,24 @@ public class ProductDetailServlet extends HttpServlet {
         }
 
         FeedbackProductDAO feedbackProductDAO = new FeedbackProductDAO();
-        List<FeedbackResponse> list=feedbackProductDAO.getAllFeedbackProduct(productID);
+        List<FeedbackResponse> list = feedbackProductDAO.getAllFeedbackProduct(productID);
+        HttpSession session = request.getSession();
+        Account account = (Account) session.getAttribute("account");
+
+        if (account != null) {
+            CustomerDAO customerDAO = new CustomerDAO();
+            Customer c = customerDAO.getCustomerByAccountID(account.getAccountID());
+            request.setAttribute("currentID", c.getCustomerID());
+        } else{
+           request.setAttribute("currentID", null);
+        }
+        for (FeedbackResponse feedback : list) {
+            List<FeedbackResponse> replies = feedbackProductDAO.getReplyComment(feedback.getFeedbackID());
+            feedback.setReplies(replies); // set danh sách replies vào feedback
+        }
         request.setAttribute("list", list);
+        request.setAttribute("productID", productID);
 
-
-        // Forward to JSP
         request.getRequestDispatcher("shop-product-full.jsp").forward(request, response);
 
     }
@@ -55,7 +68,7 @@ public class ProductDetailServlet extends HttpServlet {
 
         HttpSession session = request.getSession();
         Account account = (Account) session.getAttribute("account");
-        if (account==null) {
+        if (account == null) {
             request.setAttribute("errorMessage", "You need to login first to write feedback!.");
             request.getRequestDispatcher("shop-product-full.jsp").forward(request, response);
             return;
@@ -68,12 +81,13 @@ public class ProductDetailServlet extends HttpServlet {
         if (!feedbackProductDAO.existOrderProduct(productID, c.getCustomerID())) {
             request.setAttribute("errorMessage", "You need to order " + product.getName() + " before you can write feedback!. ");
             request.getRequestDispatcher("shop-product-full.jsp").forward(request, response);
-        }else{
-            String comment=request.getParameter("comment");
-            int rating=Integer.parseInt(request.getParameter("rating"));
-            LocalDateTime currentTime=LocalDateTime.now();
-            feedbackProductDAO.updateFeedbackProduct(productID, c.getCustomerID(), rating, comment,currentTime);
-
+        } else {
+            String comment = request.getParameter("comment");
+            int rating = Integer.parseInt(request.getParameter("rating"));
+            LocalDateTime currentTime = LocalDateTime.now();
+//          feedbackProductDAO.updateFeedbackProduct(productID, c.getCustomerID(), rating, comment,currentTime);
+            Integer replyID = null;
+            feedbackProductDAO.addFeedbackProduct(productID, c.getCustomerID(), rating, comment, currentTime, replyID);
             doGet(request, response);
 
         }
--- src/main/java/dal/FeedbackProductDAO.java
+++ src/main/java/dal/FeedbackProductDAO.java
@@ -51,16 +51,49 @@ public class FeedbackProductDAO extends DBContext {
         }
     }
 
+    public void addFeedbackProduct(int productID, int customerID, Integer star, String comment, LocalDateTime time, Integer replyID) {
+        String sql = "INSERT INTO [dbo].[FeedbackProduct]\n" +
+                "           ([ProductID],\n" +
+                "           [CustomerID],\n" +
+                "           [Star],\n" +
+                "           [Feedback],\n" +
+                "           [Time],\n" +
+                "           [ReplyID])\n" +
+                "     VALUES\n" +
+                "           (?, ?, ?, ?, ?, ?);";
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setInt(1, productID);
+            st.setInt(2, customerID);
+            if (star != null) {
+                st.setInt(3, star);
+            } else {
+                st.setNull(3, java.sql.Types.INTEGER);
+            }
+            st.setString(4, comment);
+            st.setTimestamp(5, Timestamp.valueOf(time));
+            if (replyID != null) {
+                st.setInt(6, replyID);
+            } else {
+                st.setNull(6, java.sql.Types.INTEGER);
+            }
+            st.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public List<FeedbackResponse> getAllFeedbackProduct(int productID) {
         List<FeedbackResponse> list = new ArrayList<>();
-        String sql = "SELECT a.[Name], f.Star, f.Feedback, FORMAT(f.[Time], 'yyyy-MM-dd HH:mm') AS FormattedTime \n" +
-                "FROM FeedbackProduct f \n" +
-                "JOIN Customer c ON f.CustomerID = c.CustomerID\n" +
-                "JOIN Account a ON c.AccountID = a.AccountID\n" +
-                "WHERE f.ProductID = ? \n" +
-                "  AND f.Star IS NOT NULL \n" +
-                "  AND f.Feedback IS NOT NULL \n" +
-                "  AND f.[Time] IS NOT NULL;";
+        String sql = "SELECT f.CustomerID,a.[Name], f.Star, f.Feedback, FORMAT(f.[Time], 'yyyy-MM-dd HH:mm') AS FormattedTime, f.ReplyID, f.FeedbackID \n" +
+                "                 FROM FeedbackProduct f \n" +
+                "                 JOIN Customer c ON f.CustomerID = c.CustomerID\n" +
+                "                 JOIN Account a ON c.AccountID = a.AccountID\n" +
+                "                 WHERE f.ProductID = ?\n" +
+                "                 AND f.Star IS NOT NULL \n" +
+                "                 AND f.Feedback IS NOT NULL \n" +
+                "                 AND f.[Time] IS NOT NULL \n" +
+                "                 ORDER BY f.ReplyID, f.FeedbackID";
 
         try {
             PreparedStatement st = connection.prepareStatement(sql);
@@ -68,10 +101,13 @@ public class FeedbackProductDAO extends DBContext {
             ResultSet rs = st.executeQuery();
             while (rs.next()) {
                 FeedbackResponse r = new FeedbackResponse();
+                r.setCustomerID(rs.getInt("CustomerID"));
                 r.setCustomerName(rs.getString("Name"));
                 r.setStar(rs.getInt("Star"));
                 r.setFeedback(rs.getString("Feedback"));
                 r.setTime(rs.getString("FormattedTime"));
+                r.setReplyID(rs.getInt("replyID"));
+                r.setFeedbackID(rs.getInt("feedbackID"));
                 list.add(r);
             }
 
@@ -82,14 +118,47 @@ public class FeedbackProductDAO extends DBContext {
         return list;
     }
 
+    public List<FeedbackResponse> getReplyComment(int replyID) {
+        List<FeedbackResponse> list = new ArrayList<>();
+        String sql = "SELECT f.CustomerID,a.[Name], f.Star, f.Feedback, FORMAT(f.[Time], 'yyyy-MM-dd HH:mm') AS FormattedTime, f.ReplyID, f.FeedbackID \n" +
+                "FROM FeedbackProduct f \n" +
+                "JOIN Customer c ON f.CustomerID = c.CustomerID\n" +
+                "JOIN Account a ON c.AccountID = a.AccountID\n" +
+                "WHERE f.ReplyID= ?               \n" +
+                "AND f.Feedback IS NOT NULL \n" +
+                " AND f.[Time] IS NOT NULL \n" +
+                "ORDER BY f.ReplyID, f.FeedbackID";
+
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setInt(1, replyID);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                FeedbackResponse r = new FeedbackResponse();
+                r.setCustomerID(rs.getInt("CustomerID"));
+                r.setCustomerName(rs.getString("Name"));
+                r.setStar(rs.getInt("Star"));
+                r.setFeedback(rs.getString("Feedback"));
+                r.setTime(rs.getString("FormattedTime"));
+                r.setReplyID(rs.getInt("replyID"));
+                r.setFeedbackID(rs.getInt("feedbackID"));
+                list.add(r);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return list;
+    }
 
 
 
     public static void main(String[] args) {
-        FeedbackProductDAO dao=new FeedbackProductDAO();
+        FeedbackProductDAO dao = new FeedbackProductDAO();
         List<FeedbackResponse> list=dao.getAllFeedbackProduct(2);
         System.out.println(list);
+
+        }
     }
 
 
-}
+
