3b7e8c677c514b2a5791d86b8698ed25f045122b
QuangAnh <anhdq2109@gmail.com>
2024-11-08 19:24:24 +0700

    fix time format

--- src/main/java/dal/NewsDAO.java
+++ src/main/java/dal/NewsDAO.java
@@ -18,7 +18,7 @@ public class NewsDAO extends DBContext {
                 rs.getInt("StaffID"),
                 rs.getString("Title"),
                 rs.getInt("ImgID"),
-                rs.getTimestamp("Time"),
+                rs.getTimestamp("Time").toLocalDateTime(),
                 rs.getString("Content"),
                 rs.getBoolean("Active")
         );
@@ -30,7 +30,7 @@ public class NewsDAO extends DBContext {
             st.setInt(1, news.getStaffID());
             st.setString(2, news.getTitle());
             st.setInt(3, news.getImgID());
-            st.setTimestamp(4, new Timestamp(news.getTime().getTime()));
+            st.setTimestamp(4, Timestamp.valueOf(news.getTime()));
             st.setString(5, news.getContent());
             st.setBoolean(6, news.getActive());
             return st.executeUpdate() > 0;
@@ -75,7 +75,7 @@ public class NewsDAO extends DBContext {
             st.setInt(1, news.getStaffID());
             st.setString(2, news.getTitle());
             st.setInt(3, news.getImgID());
-            st.setTimestamp(4, new Timestamp(news.getTime().getTime()));
+            st.setTimestamp(4, Timestamp.valueOf(news.getTime()));
             st.setString(5, news.getContent());
             st.setBoolean(6, news.getActive());
             st.setInt(7, news.getNewsID());
